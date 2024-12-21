dba883ab84a7cfac83cb1c1d709a31a5cb114c26
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-12-21 09:01:26 +0700

    Initial commit

---
+++ src/main/java/com/example/springsecs01/Springsecs01Application.java
@@ -0,0 +1,15 @@
+package com.example.springsecs01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecs01Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Springsecs01Application.class, args);
+		System.out.println("running...");
+	}
+
+}
---
+++ src/test/java/com/example/springsecs01/Springsecs01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecs01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecs01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
