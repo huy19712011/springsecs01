b8816fdbdd60f4f56a3c375acf1826122738f5ce
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-12-22 10:24:57 +0700

    V18: Checking default configuration inside security framework

---
+++ src/main/java/com/example/controller/ProjectSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.controller;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        System.out.println("custom security filter chain");
+        return http.build();
+    }
+}
